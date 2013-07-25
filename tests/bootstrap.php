97ed5a0a3dea1ee9f02fbbd06d715b2eeb6b7807
Diego Zanella <businessdad75@gmail.com>
2013-07-25 21:33:49 +0100

    Corrected directory for Composer autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,7 +11,7 @@ if(file_exists($path)) {
 }
 
 // Load Composer Autoloader
-$composer_autoloader = '../vendor/autoload.php';
+$composer_autoloader = realpath(__DIR__ . '/../vendor/autoload.php');
 if(file_exists($composer_autoloader)) {
   require_once $composer_autoloader;
 } else {
