2adab153028febaeda5e8b3667c69d78deb14344
Diego Zanella <businessdad75@gmail.com>
2013-07-25 21:22:04 +0100

    Added loading of composer autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,15 @@
 $path = '/src/wp_unit/bootstrap.php';
 
 if(file_exists($path)) {
-    require_once $path;
+  require_once $path;
 } else {
-    exit("Couldn't find path to wp_unit bootstrap.php. Expected location: '$path'.\n");
+   exit("Couldn't find path to wp_unit bootstrap.php. Expected location: '$path'.\n");
+}
+
+// Load Composer Autoloader
+$composer_autoloader = '../vendor/autoload.php';
+if(file_exists($composer_autoloader)) {
+  require_once $composer_autoloader;
+} else {
+   exit("Couldn't find path to composer autoloader. Expected location: '$composer_autoloader'.\n");
 }
