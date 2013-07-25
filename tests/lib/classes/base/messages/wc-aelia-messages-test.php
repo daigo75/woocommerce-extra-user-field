4e864a58624c5b5023fabba94cfe98f9131d66bc
Diego Zanella <businessdad75@gmail.com>
2013-07-25 21:51:17 +0100

    Corrected class declaration

--- tests/lib/classes/base/messages/wc-aelia-messages-test.php
+++ tests/lib/classes/base/messages/wc-aelia-messages-test.php
@@ -2,7 +2,7 @@
 
 require_once 'plugin-template.php';
 
-class WC_Aelia_Messages extends WP_UnitTestCase {
+class WC_Aelia_Messages_Test extends WP_UnitTestCase {
 	const MSG_CODE = 'test_msg_code';
 	const MESSAGE = 'Test Message';
 
@@ -21,4 +21,3 @@ class WC_Aelia_Messages extends WP_UnitTestCase {
 		$this->assertEquals($this->aelia_messages->get_error_message(self::MSG_CODE), self::MESSAGE);
 	}
 }
-
