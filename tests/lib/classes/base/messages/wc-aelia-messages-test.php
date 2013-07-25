b6010988fdc6ec8e71f56e4223871f4cc8ffeba8
Diego Zanella <businessdad75@gmail.com>
2013-07-25 22:09:45 +0100

    Corrected test for WC_Aelia_Messages_Test::test_load_error_messages()

--- tests/lib/classes/base/messages/wc-aelia-messages-test.php
+++ tests/lib/classes/base/messages/wc-aelia-messages-test.php
@@ -26,6 +26,7 @@ class WC_Aelia_Messages_Test extends WP_UnitTestCase {
 
 	function test_load_error_messages() {
 		$this->aelia_messages->load_error_messages();
-		$this->assertTrue($this->aelia_messages->get_error_message(WC_Aelia_Messages::ERR_FILE_NOT_FOUND));
+		$message = $this->aelia_messages->get_error_message(WC_Aelia_Messages::ERR_FILE_NOT_FOUND);
+		$this->assertTrue(!empty($message));
 	}
 }
