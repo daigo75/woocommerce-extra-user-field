cb17276dcc1f9b7a15fab9e48417fbf7591f012b
Diego Zanella <businessdad75@gmail.com>
2013-08-29 22:51:21 +0100

    Added settings controller and settings page renderer classes

--- src/lib/classes/base/settings/wc-aelia-settings.php
+++ src/lib/classes/base/settings/wc-aelia-settings.php
@@ -5,4 +5,142 @@
  * plugin.
  */
 class WC_Aelia_Settings {
+	// @var string The text domain to be used for localisation.
+	protected $_textdomain = '';
+	// @var string The key to identify plugin settings amongst WP options.
+	protected $_settings_key;
+	// @var string The renderer used to render the settings page.
+	protected $_renderer;
+
+	// @var int Value indicating ifa feature was enabled
+	const ENABLED_YES = 1;
+	// @var int Value indicating ifa feature was disabled
+	const ENABLED_NO = 0;
+
+	// @var array Stores current plugin settings.
+	protected $_current_settings;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param string settings_key The key used to store and retrieve the plugin settings.
+	 * @param string textdomain The text domain used for localisation.
+	 * @param string renderer The renderer to use to generate the settings page.
+	 * @return WC_Aelia_Settings.
+	 */
+	public function __construct($settings_key, $textdomain = '', WC_Aelia_Settings_Renderer $renderer) {
+		$this->_settings_key = $settings_key;
+		$this->_textdomain = $textdomain;
+		$this->_renderer = $renderer;
+	}
+
+	/**
+	 * Returns the default settings for the plugin. Used mainly at first
+	 * installation.
+	 *
+	 * @param string key If specified, method will return only the setting identified
+	 * by the key.
+	 * @param mixed default The default value to return if the setting requested
+	 * via the "key" argument is not found.
+	 * @return array|mixed The default settings, or the value of the specified
+	 * setting.
+	 */
+	public function default_settings($key = null, $default = null) {
+		return null;
+	}
+
+	/**
+	 * Returns current plugin settings, or the value a specific setting.
+	 *
+	 * @param string key If specified, method will return only the setting identified
+	 * by the key.
+	 * @param mixed default The default value to return if the setting requested
+	 * via the "key" argument is not found.
+	 * @return array|mixed The plugin settings, or the value of the specified
+	 * setting.
+	 */
+	public function current_settings($key = null, $default = null) {
+		if(empty($this->_current_settings)) {
+			$this->_current_settings = $this->load();
+		}
+
+		if(empty($key)) {
+			return $this->_current_settings;
+		}
+		else {
+			return get_value($key, $this->_current_settings, $default);
+		}
+	}
+
+	/**
+	 * Loads plugin settings from WP database.
+	 *
+	 * @return array An array containing the plugin settings.
+	 */
+	public function load() {
+		return get_option($this->_settings_key);
+	}
+
+	/**
+	 * Saves plugin settings to WP database. Existing settings are merged with
+	 * the ones passed as a parameter.
+	 *
+	 * @param array settings An array of settings.
+	 */
+	public function save(array $settings = array()) {
+		$current_settings = $this->load();
+		$current_settings = empty($current_settings) ? $this->default_settings() : $current_settings;
+
+		$settings = array_merge($current_settings, $settings);
+
+		update_option($this->_settings_key, $settings);
+		// Invalidate cached settings, so that they will be loaded again when requested
+		unset($this->_current_settings);
+	}
+
+	/**
+	 * Deletes plugin settings from WP database.
+	 */
+	public function delete() {
+		delete_option($this->_settings_key);
+	}
+
+	/**
+	 * Initialises plugin's settings.
+	 */
+	public function init_settings() {
+		// Prepare settings page for rendering
+		$this->_renderer->init_settings_page();
+
+	  // Register settings.
+		register_setting($this->_settings_key, $this->_settings_key, array($this, 'validate_settings'));
+	}
+
+	/**
+	 * Validates the settings specified via the Options page. This method should
+	 * be overridden by descendant classes.
+	 *
+	 * @param array settings An array of settings.
+	 */
+	public function validate_settings($settings) {
+		return $settings;
+	}
+
+	public function options_page_load() {
+		if(get_value('settings-updated', $_GET)) {
+      //plugin settings have been saved. Here goes your code
+			//var_dump('Settings saved.');
+		}
+	}
+
+	/**
+	 * Given an array of Code => Message pairs, adds them to the settings errors.
+	 *
+	 * @param array errors An array of Code => Message pairs.
+	 */
+	protected function add_multiple_settings_errors(array $errors) {
+		foreach($errors as $code => $message) {
+			add_settings_error($this->_settings_key, $code, $message);
+		}
+	}
 }
