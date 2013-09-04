d09e1f1e008c11726ba92d4a126830d9851693b4
Diego Zanella <businessdad75@gmail.com>
2013-09-04 22:34:39 +0100

    Added tests for Settings controller

--- tests/lib/classes/base/settings/wc-aelia-settings-test.php
+++ tests/lib/classes/base/settings/wc-aelia-settings-test.php
@@ -4,8 +4,22 @@
  * Tests for the base settings controller.
  */
 class WC_Aelia_Settings_Test {
-	const SETTINGS_KEY = 'test';
-	const TEXT_DOMAIN = 'test';
+	const SETTINGS_KEY = 'test_settings';
+	const TEXT_DOMAIN = 'test_domain';
+
+	protected function get_test_settings($key = null) {
+		$test_settings = array(
+			'int_param' => 1,
+			'string_param' => 'string_val',
+		);
+
+		if($key === null) {
+			return $test_settings;
+		}
+		else {
+			return $test_settings[$key];
+		}
+	}
 
 	public function setUp() {
 		parent::setUp();
@@ -14,5 +28,24 @@ class WC_Aelia_Settings_Test {
 		$this->settings = new WC_Aelia_Settings(self::SETTINGS_KEY,
 																						self::TEXT_DOMAIN,
 																						$renderer);
+	}
+
+	public function test_default_settings() {
+		$this->assertTrue($this->settings->default_settings() === null);
+	}
+
+	public function test_save() {
+		$this->settings->save($this->get_test_settings());
+		$this->assertEqual($this->settings->load(), $this->test_settings());
+	}
+
+	public function test_load() {
+		$this->assertEqual($this->settings->load(), $this->test_settings());
+	}
+
+	public function test_current_settings() {
+		$test_settings = $this->get_test_settings('string_param');
+		$current_settings = $this->settings->current_settings('string_param');
+		$this->assertEqual($current_settings, $test_settings);
 	}
 }
