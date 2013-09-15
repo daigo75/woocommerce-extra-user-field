d9fb9c7e2d5e32c1ef814aa438333fdc4cb073bd
Diego Zanella <businessdad75@gmail.com>
2013-09-15 16:28:43 +0100

    Corrected initialisation of plugin

--- src/plugin-main.php
+++ src/plugin-main.php
@@ -16,7 +16,35 @@ require_once(__DIR__ . '/vendor/autoload.php');
 /**
  * Localisation
  **/
-class Template_Plugin extends WC_Aelia_Plugin {
+class WC_Aelia_Template_Plugin extends WC_Aelia_Plugin {
+	/**
+	 * Constructor.
+	 *
+	 * @param WC_Aelia_Settings settings_controller The controller that will handle
+	 * the plugin settings.
+	 * @param WC_Aelia_Messages messages_controller The controller that will handle
+	 * the messages produced by the plugin.
+	 */
+	public function __construct(WC_Aelia_Settings $settings_controller,
+															WC_Aelia_Messages $messages_controller) {
+		parent::__construct($settings_controller, $messages_controller);
+
+		self::$plugin_slug = 'wc-authipay-gateway-plugin';
+		self::$text_domain = self::$plugin_slug;
+		self::$settings_key = self::$plugin_slug;
+		self::$instance_key = self::$plugin_slug;
+	}
+
+	public static function factory() {
+		$settings_page_renderer = new WC_Aelia_Settings_Renderer();
+		$settings_controller = new WC_Aelia_Settings(self::SETTINGS_KEY,
+																								 self::TEXT_DOMAIN,
+																								 $settings_page_renderer);
+		$messages_controller = new WC_Aelia_Messages();
+
+		$plugin_instance = new WC_Aelia_Template_Plugin($settings_controller, $messages_controller);
+		return $plugin_instance;
+	}
 }
 
-$GLOBALS['template_plugin'] = new Template_Plugin();
+$GLOBALS['template_plugin'] = WC_Aelia_Template_Plugin::factory();
