64954e9589ef47d600f0c71999bab50b3c9af8ae
Diego Zanella <businessdad75@gmail.com>
2013-09-17 00:26:39 +0100

    Debug

--- src/lib/classes/base/plugin/wc-aelia-plugin.php
+++ src/lib/classes/base/plugin/wc-aelia-plugin.php
@@ -202,9 +202,8 @@ class WC_Aelia_Plugin implements IWC_Aelia_Plugin {
 		if(empty($this->plugin_directory)) {
 			$reflector = new ReflectionClass($this);
 			$this->plugin_directory = basename(dirname(dirname($reflector->getFileName())));
-		}
-
 		var_dump($reflector->getFileName());
+		}
 
 		return $this->plugin_directory;
 	}
