21190166514670521470032954cec6d5c5c46a34
Diego Zanella <businessdad75@gmail.com>
2013-09-23 15:21:23 +0100

    Corrected information

--- plugin-template.php
+++ plugin-template.php
@@ -1,9 +1,9 @@
 <?php if(!defined('ABSPATH')) exit; // Exit if accessed directly
 /*
-Plugin Name: WP Plugin Template
-Description: This is a WordPress plugin template
-Author: Your Name
-Version: 0.1
+Plugin Name: WooCommerce Plugin Template
+Description: This is a WooCommerce plugin template
+Author: Diego Zanella
+Version: 0.2
 */
 
 require_once 'src/plugin-main.php';
