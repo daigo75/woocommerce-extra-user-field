a1703cd7b4f991550ab7d4f7bdbfe87ba95d9631
Diego Zanella <businessdad75@gmail.com>
2014-01-12 14:04:25 +0000

    Added session manager

---
+++ src/lib/classes/base/session/aelia-session-manager.php
@@ -0,0 +1,77 @@
+<?php if(!defined('ABSPATH')) exit; // Exit ifaccessed directly
+
+if(class_exists('Aelia_SessionManager')) {
+	return;
+}
+
+/**
+ * A simple Session handler. Compatible with both WooCommerce 2.0 and earlier.
+ */
+class Aelia_SessionManager {
+	/**
+	 * Safely store data into the session. Compatible with WooCommerce 2.0+ and
+	 * backwards compatible with previous versions.
+	 *
+	 * @param string key The Key of the value to retrieve.
+	 * @param mixed value The value to set.
+	 */
+	public static function set_value($key, $value) {
+		global $woocommerce;
+
+		if($woocommerce->version >= '2') {
+			// WooCommerce 2.0+
+			$woocommerce->session->$key = $value;
+		} else {
+			// WooCommerce < 2.0
+			$_SESSION[$key] = $value;
+		}
+	}
+
+	/**
+	 * Safely retrieve data from the session. Compatible with WooCommerce 2.0+ and
+	 * backwards compatible with previous versions.
+	 *
+	 * @param string key The Key of the value to retrieve.
+	 * @param mixed default The default value to return if the key is not found.
+	 * @param bool remove_after_get Indicates if the value should be removed after
+	 * having been retrieved.
+	 * @return mixed The value associated with the key, or the default.
+	 */
+	public static function get_value($key, $default = null, $remove_after_get = false) {
+		global $woocommerce;
+
+		if($woocommerce->version >= '2') {
+			// WooCommerce 2.0+
+			if(!isset($woocommerce->session)) {
+				return $default;
+			}
+			$result = @$woocommerce->session->$key;
+		} else {
+			// WooCommerce < 2.0
+			$result = @$_SESSION[$key];
+		}
+		if($remove_after_get) {
+			self::delete_value($key);
+		}
+
+		return empty($result) ? $default : $result;
+	}
+
+	/**
+	 * Safely remove data from the session. Compatible with WooCommerce 2.0+ and
+	 * backwards compatible with previous versions.
+	 *
+	 * @param string key The Key of the value to retrieve.
+	 */
+	public static function delete_value($key) {
+		global $woocommerce;
+
+		if($woocommerce->version >= '2') {
+			// WooCommerce 2.0+
+			unset($woocommerce->session->$key);
+		} else {
+			// WooCommerce < 2.0
+			unset($_SESSION[$key]);
+		}
+	}
+}
