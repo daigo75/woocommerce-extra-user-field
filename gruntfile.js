214340c3e62eca8e6366120139696e23f031dc54
Diego Zanella <businessdad75@gmail.com>
2015-05-13 21:22:13 +0100

    *** WIP ** Implementing Grunt script

--- gruntfile.js
+++ gruntfile.js
@@ -3,36 +3,64 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		'string-replace': {
+			// Replace each plugin token with the corresponding value from configuration
 			'plugin-tokens': {
 				files: {
-					'./': ['*', 'src/**', 'tests/**'],
+					'./': ['plugin-template.php'],
 				},
 				options: {
 					replacements: [
 						{
-							pattern: function() {
-								var tokens = [];
-								for(var token in pkg.plugin_info) {
-									tokens.push(token);
-								}
-								var result = '/' + tokens.join('|') + '/g';
-								grunt.log.write(result);
-								return result;
-							},
+							pattern: /(PLUGIN_NAME|PLUGIN_DESCRIPTION|PLUGIN_VERSION|PLUGIN_AUTHOR|PLUGIN_AUTH_URI|PLUGIN_LICENSE|PLUGIN_NAMESPACE|PLUGIN_CLASS_NAME|PLUGIN_SLUG_VALUE|PLUGIN_MENU_SLUG|PLUGIN_TEXT_DOMAIN|PLUGIN_SUPPORT_URL|PLUGIN_CONTACT_URL|PLUGIN_SETTINGS_KEY|PLUGIN_FILTER_PREFIX)/g,
 							replacement: function(match, p1) {
-								var plugin_info = grunt.config.get('pkg.plugin_info');
+								var config = grunt.config.get('pkg');
+								var plugin_info = config.plugin_info;
+								plugin_info['PLUGIN_DESCRIPTION'] = config.description;
+								plugin_info['PLUGIN_VERSION'] = config.version;
+								plugin_info['PLUGIN_AUTHOR'] = config.author.name;
+								plugin_info['PLUGIN_AUTH_URI'] = ('url' in config.author) ? config.author.url : '';
+								plugin_info['PLUGIN_LICENSE'] = config.license;
+
+								//grunt.log.writeln("MATCH: " + match);
+								//grunt.log.writeln("P1: " + p1);
 								return (match in plugin_info) ? plugin_info[match] : ("NOT_FOUND_" + match);
 							}
 						}
 					]
 				}
+			},
+			// Replace the reference to the "plugin-template.php" file with the name
+			// of the main plugin file (see rename:main-plugin-file task).
+			'main-plugin-file-references': {
+				files: {
+					'./': ['src/**', 'tests/**'],
+				},
+				options: {
+					replacements: [
+						{
+							pattern: 'plugin-template.php',
+							replacement: '<%= pkg.plugin_info["PLUGIN_SLUG_VALUE"] %>.php'
+						}
+					]
+				}
+			}
+		},
+		// Rename the main plugin file. The file name will match the plugin slug.
+		'rename': {
+			'main-plugin-file': {
+				src: './plugin-template.php',
+				dest: './<%= pkg.plugin_info["PLUGIN_SLUG_VALUE"] %>.php'
 			}
 		}
 	});
 
-	// Load the plugin that will replace the PLUGIN_* tokens with the actual values
 	grunt.loadNpmTasks('grunt-string-replace');
+	grunt.loadNpmTasks('grunt-rename');
 
 	// Default task(s).
-  grunt.registerTask('default', ['string-replace:plugin-tokens']);
+  grunt.registerTask('default', [
+		'string-replace:plugin-tokens',
+		'string-replace:main-plugin-file-references',
+		'rename:main-plugin-file',
+	]);
 };
