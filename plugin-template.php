0a662d93625f9fd74c616b995f9dc237a0c00848
Diego Zanella <businessdad75@gmail.com>
2015-05-13 22:01:30 +0100

    Fixed reference to requirement checking file

--- plugin-template.php
+++ plugin-template.php
@@ -6,7 +6,7 @@ Author: PLUGIN_AUTHOR
 Version: PLUGIN_VERSION
 */
 
-require_once(dirname(__FILE__) . '/src/lib/classes/install/aelia-wc-plugin-slug-requirementscheck.php');
+require_once(dirname(__FILE__) . '/src/lib/classes/install/plugin-slug-requirementscheck.php');
 // If requirements are not met, deactivate the plugin
 if(PLUGIN_CLASS_NAME_RequirementsChecks::factory()->check_requirements()) {
 	require_once dirname(__FILE__) . '/src/plugin-main.php';
