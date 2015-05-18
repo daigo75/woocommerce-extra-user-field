a037bde1a2cfb87b8bcd3bf6a7a6a5089c5e73f0
Diego Zanella <businessdad75@gmail.com>
2015-05-18 21:11:26 +0100

    Added plugin token to Admin JavaScript template code

--- src/js/admin/admin.js
+++ src/js/admin/admin.js
@@ -1,4 +1,11 @@
 /* JavaScript for Admin pages */
 jQuery(document).ready(function($) {
 	// Here jQuery can be accessed using the "$" variable
+
+	// Uncomment as needed
+	//var $settings_form = $('#PLUGIN_SETTINGS_KEY_form');
+	//// If form is not found, we are not on this plugin's setting page
+	//if(!$settings_form.length) {
+	//	return;
+	//}
 });
